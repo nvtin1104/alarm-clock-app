93a49c237e31a995fefb84c68ec432591f83418e
Nguyễn Văn Tín <bguyt46@gmail.com>
2025-05-05 22:13:29 +0700

    build: clear electron

--- electron/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electron', {
-  sendNotification: (title, body) =>
-    ipcRenderer.send('show-notification', { title, body }),
-});
