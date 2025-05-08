459c28fd7794ee7e7f11784ab412d4b8fe50e530
Nguyễn Văn Tín <bguyt46@gmail.com>
2025-05-08 08:53:17 +0700

    fix: sửa lỗi khởi động và traysystem

--- alarm-clock/src/App.jsx
+++ alarm-clock/src/App.jsx
@@ -1,7 +1,5 @@
 import AlarmNotifier from "./components/AlarmNotifier";
 import { enable, isEnabled, disable } from '@tauri-apps/plugin-autostart';
-import { TrayIcon } from '@tauri-apps/api/tray';
-import { Menu } from "@tauri-apps/api/menu";
 await enable();
 
 // const tray = await TrayIcon.new(options);
